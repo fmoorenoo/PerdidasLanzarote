2558f21ae8313320af9fb6a1db8e3475b53c59fe
fmoorenoo <fermontemore@gmail.com>
2024-12-13 11:33:44 +0000

    Atributos para la tabla 'item_type'

--- app/src/main/java/org/iesharia/perdidaslanzarote/model/entities/ItemType.kt
+++ app/src/main/java/org/iesharia/perdidaslanzarote/model/entities/ItemType.kt
@@ -1,6 +1,15 @@
 package org.iesharia.perdidaslanzarote.model.entities
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(
+    tableName = "item_type"
+)
+
+data class ItemType(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String,
+    val numItems: Int
 )
