ae7c91a76cf630ed95c990c4cd545e0ed1eac8e1
fmoorenoo <fermontemore@gmail.com>
2024-12-13 10:40:42 +0000

    Atributos para la tabla 'lost_item'

--- app/src/main/java/org/iesharia/perdidaslanzarote/model/entities/LostItem.kt
+++ app/src/main/java/org/iesharia/perdidaslanzarote/model/entities/LostItem.kt
@@ -1,5 +1,17 @@
 package org.iesharia.perdidaslanzarote.model.entities
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-@Entity()
+@Entity(
+    tableName = "lost_item"
+)
+
+data class LostItem(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val itemName: String,
+    val description: String? = null,
+    val placeId: Int,
+    val itemTypeId: Int
+)
