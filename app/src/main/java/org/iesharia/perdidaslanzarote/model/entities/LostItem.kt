8cef5f37ff368aaee0be471edf9dde57b71027af
fmoorenoo <fermontemore@gmail.com>
2024-12-13 10:42:05 +0000

    Añadidas Foreign Keys necesarias para la tabla 'lost_item'

--- app/src/main/java/org/iesharia/perdidaslanzarote/model/entities/LostItem.kt
+++ app/src/main/java/org/iesharia/perdidaslanzarote/model/entities/LostItem.kt
@@ -2,9 +2,26 @@ package org.iesharia.perdidaslanzarote.model.entities
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.ForeignKey
 
 @Entity(
-    tableName = "lost_item"
+    tableName = "lost_item",
+    foreignKeys = [
+        ForeignKey(
+            entity = Place::class,
+            // Referencia en la tabla Place
+            parentColumns = ["id"],
+            // Referencia en esta tabla
+            childColumns = ["placeId"],
+        ),
+        ForeignKey(
+            entity = ItemType::class,
+            // Referencia en la tabla ItemType
+            parentColumns = ["id"],
+            // Referencia en esta tabla
+            childColumns = ["itemTypeId"],
+        )
+    ]
 )
 
 data class LostItem(
