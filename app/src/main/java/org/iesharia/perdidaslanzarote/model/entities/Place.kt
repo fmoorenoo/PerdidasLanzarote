29f283c3d97d8fe9053ebea14a595faa3fd5625c
fmoorenoo <fermontemore@gmail.com>
2024-12-13 11:25:48 +0000

    Atributos para la tabla 'place'

--- app/src/main/java/org/iesharia/perdidaslanzarote/model/entities/Place.kt
+++ app/src/main/java/org/iesharia/perdidaslanzarote/model/entities/Place.kt
@@ -1,6 +1,17 @@
 package org.iesharia.perdidaslanzarote.model.entities
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(
+    tableName = "place",
+)
+
+data class Place(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String,
+    val longitude: String,
+    val latitude: String,
+    val numItems: Int
 )
