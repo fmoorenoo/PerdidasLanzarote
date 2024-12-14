5b9e743afe635cd23b0692fce992fe33205c3f60
fmoorenoo <fermontemore@gmail.com>
2024-12-14 19:36:40 +0000

    Método para obtener todos los lugares

--- app/src/main/java/org/iesharia/perdidaslanzarote/model/dao/PlaceDAO.kt
+++ app/src/main/java/org/iesharia/perdidaslanzarote/model/dao/PlaceDAO.kt
@@ -1,6 +1,11 @@
 package org.iesharia.perdidaslanzarote.model.dao
 
 import androidx.room.Dao
+import androidx.room.Query
+import org.iesharia.perdidaslanzarote.model.entities.Place
 
 @Dao
-interface PlaceDao{}
+interface PlaceDao {
+    @Query("SELECT * FROM place")
+    fun getAllPlaces(): List<Place>
+}
