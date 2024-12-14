88e112547cad84bac9de9668919d6dc7274432ee
fmoorenoo <fermontemore@gmail.com>
2024-12-14 19:54:17 +0000

    Inicializada base de datos y el ViewModel

--- app/src/main/java/org/iesharia/perdidaslanzarote/MainActivity.kt
+++ app/src/main/java/org/iesharia/perdidaslanzarote/MainActivity.kt
@@ -3,15 +3,22 @@ package org.iesharia.perdidaslanzarote
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import org.iesharia.perdidaslanzarote.model.AppDatabase
 import org.iesharia.perdidaslanzarote.view.MainApp
+import org.iesharia.perdidaslanzarote.viewmodel.AppViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        // Inicializar la base de datos con los DAO y el ViewModel
+        val database = AppDatabase.getDatabase(this)
+        val lostItemDao = database.lostItemDao()
+        val itemTypeDao = database.itemTypeDao()
+        val placeDao = database.placeDao()
+
+        val mapViewModel = AppViewModel(lostItemDao, itemTypeDao, placeDao)
         setContent {
-            MainApp()
+            MainApp(mapViewModel)
         }
     }
 }
