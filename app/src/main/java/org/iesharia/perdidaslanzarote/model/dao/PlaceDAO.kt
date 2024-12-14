6da97ac957756c8ef5862c92d705da46de51abd0
fmoorenoo <fermontemore@gmail.com>
2024-12-14 19:50:54 +0000

    Método '@insert' para añadir lugares

--- app/src/main/java/org/iesharia/perdidaslanzarote/model/dao/PlaceDAO.kt
+++ app/src/main/java/org/iesharia/perdidaslanzarote/model/dao/PlaceDAO.kt
@@ -1,6 +1,7 @@
 package org.iesharia.perdidaslanzarote.model.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
 import org.iesharia.perdidaslanzarote.model.entities.Place
 
@@ -8,4 +9,7 @@ import org.iesharia.perdidaslanzarote.model.entities.Place
 interface PlaceDao {
     @Query("SELECT * FROM place")
     fun getAllPlaces(): List<Place>
+
+    @Insert
+    suspend fun insertPlace(place: Place)
 }
