f74ed4e81e791223684d568f2a5992a4a35e0fdb
fmoorenoo <fermontemore@gmail.com>
2024-12-14 20:09:58 +0000

    Uso de Flow para manejo reactivo de los datos

--- app/src/main/java/org/iesharia/perdidaslanzarote/model/dao/ItemTypeDAO.kt
+++ app/src/main/java/org/iesharia/perdidaslanzarote/model/dao/ItemTypeDAO.kt
@@ -3,12 +3,13 @@ package org.iesharia.perdidaslanzarote.model.dao
 import androidx.room.Dao
 import androidx.room.Query
 import androidx.room.Insert
+import kotlinx.coroutines.flow.Flow
 import org.iesharia.perdidaslanzarote.model.entities.ItemType
 
 @Dao
 interface ItemTypeDao {
     @Query("SELECT * FROM item_type")
-    fun getAllItemTypes(): List<ItemType>
+    fun getAllItemTypes(): Flow<List<ItemType>>
 
     @Insert
     suspend fun insertItemType(itemType: ItemType)
--- app/src/main/java/org/iesharia/perdidaslanzarote/model/dao/PlaceDAO.kt
+++ app/src/main/java/org/iesharia/perdidaslanzarote/model/dao/PlaceDAO.kt
@@ -3,12 +3,13 @@ package org.iesharia.perdidaslanzarote.model.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 import org.iesharia.perdidaslanzarote.model.entities.Place
 
 @Dao
 interface PlaceDao {
     @Query("SELECT * FROM place")
-    fun getAllPlaces(): List<Place>
+    fun getAllPlaces(): Flow<List<Place>>
 
     @Insert
     suspend fun insertPlace(place: Place)
