06480276533d5662e429d0428bfb94a490260b79
fmoorenoo <fermontemore@gmail.com>
2024-12-14 19:41:48 +0000

    Método '@insert' para añadir tipos de pérdidas

--- app/src/main/java/org/iesharia/perdidaslanzarote/model/dao/ItemTypeDAO.kt
+++ app/src/main/java/org/iesharia/perdidaslanzarote/model/dao/ItemTypeDAO.kt
@@ -2,10 +2,14 @@ package org.iesharia.perdidaslanzarote.model.dao
 
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.Insert
 import org.iesharia.perdidaslanzarote.model.entities.ItemType
 
 @Dao
 interface ItemTypeDao {
     @Query("SELECT * FROM item_type")
     fun getAllItemTypes(): List<ItemType>
+
+    @Insert
+    suspend fun insertItemType(itemType: ItemType)
 }
