190bf0a8d080e4eb44d9d6a00e50cea4c723b989
fmoorenoo <fermontemore@gmail.com>
2024-12-14 19:35:18 +0000

    Método para obtener todos los tipos de pérdidas

--- app/src/main/java/org/iesharia/perdidaslanzarote/model/dao/ItemTypeDAO.kt
+++ app/src/main/java/org/iesharia/perdidaslanzarote/model/dao/ItemTypeDAO.kt
@@ -1,6 +1,11 @@
 package org.iesharia.perdidaslanzarote.model.dao
 
 import androidx.room.Dao
+import androidx.room.Query
+import org.iesharia.perdidaslanzarote.model.entities.ItemType
 
 @Dao
-interface ItemTypeDao{}
+interface ItemTypeDao {
+    @Query("SELECT * FROM item_type")
+    fun getAllItemTypes(): List<ItemType>
+}
