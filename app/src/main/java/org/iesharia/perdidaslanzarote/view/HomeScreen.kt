a3f89a7840ff9ee2d05256110cb2f082262fc0e0
fmoorenoo <fermontemore@gmail.com>
2024-12-14 18:18:16 +0000

    Card creado con un Text para el título

--- app/src/main/java/org/iesharia/perdidaslanzarote/view/HomeScreen.kt
+++ app/src/main/java/org/iesharia/perdidaslanzarote/view/HomeScreen.kt
@@ -3,13 +3,19 @@ package org.iesharia.perdidaslanzarote.view
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeScreen() {
@@ -20,5 +26,22 @@ fun HomeScreen() {
             .padding(20.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Card(
+            modifier = Modifier.fillMaxWidth().padding(5.dp),
+            colors = CardDefaults.cardColors(containerColor = Color(0xFFA7B1FF)),
+            shape = RoundedCornerShape(15.dp)
+        ) {
+            Column(
+                modifier = Modifier.padding(16.dp).fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Registrar una pérdida",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    color = Color.Black
+                )
+            }
+        }
     }
 }
