cb3da6f7fdffe1477c1739959e1c4db5e6b248e1
fmoorenoo <fermontemore@gmail.com>
2024-12-14 19:26:30 +0000

    Método '@insert' para insertar pérdidas en la base de datos

--- app/src/main/java/org/iesharia/perdidaslanzarote/model/dao/LostItemDAO.kt
+++ app/src/main/java/org/iesharia/perdidaslanzarote/model/dao/LostItemDAO.kt
@@ -1,6 +1,11 @@
 package org.iesharia.perdidaslanzarote.model.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import org.iesharia.perdidaslanzarote.model.entities.LostItem
 
 @Dao
-interface LostItemDao{}
+interface LostItemDao {
+    @Insert
+    suspend fun insertLostItem(lostItem: LostItem)
+}
