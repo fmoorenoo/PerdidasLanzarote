ffca42a9ce842fd9fd4e65aaa9d94fcb8b68e553
fmoorenoo <fermontemore@gmail.com>
2024-12-14 19:39:04 +0000

    Función para añadir pérdidas a la base de datos

--- app/src/main/java/org/iesharia/perdidaslanzarote/viewmodel/AppViewModel.kt
+++ app/src/main/java/org/iesharia/perdidaslanzarote/viewmodel/AppViewModel.kt
@@ -1,8 +1,29 @@
 package org.iesharia.perdidaslanzarote.viewmodel
 
 import androidx.lifecycle.ViewModel
-import org.iesharia.perdidaslanzarote.model.entities.*
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.iesharia.perdidaslanzarote.model.dao.*
+import org.iesharia.perdidaslanzarote.model.entities.*
 
-class AppViewModel {
+
+class AppViewModel(
+    private val lostItemDao: LostItemDao,
+    private val itemTypeDao: ItemTypeDao,
+    private val placeDao: PlaceDao
+) : ViewModel() {
+
+    // Añadir una nueva pérdida
+    fun addLostItem(itemName: String, itemTypeId: Int, description: String?, placeId: Int) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val newLostItem = LostItem(
+                itemName = itemName,
+                itemTypeId = itemTypeId,
+                description = description,
+                placeId = placeId
+            )
+            lostItemDao.insertLostItem(newLostItem)
+        }
+    }
 }
