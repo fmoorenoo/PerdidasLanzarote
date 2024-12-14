33f1573510420dde17c4fecebeb961624cf795ef
fmoorenoo <fermontemore@gmail.com>
2024-12-14 19:21:05 +0000

    Creado AppViewModel.kt en el directorio 'viewmodel'

---
+++ app/src/main/java/org/iesharia/perdidaslanzarote/viewmodel/AppViewModel.kt
@@ -0,0 +1,8 @@
+package org.iesharia.perdidaslanzarote.viewmodel
+
+import androidx.lifecycle.ViewModel
+import org.iesharia.perdidaslanzarote.model.entities.*
+import org.iesharia.perdidaslanzarote.model.dao.*
+
+class AppViewModel {
+}
