4ae5b08bb6fd626033226f31979fe8cbc13615ca
fmoorenoo <fermontemore@gmail.com>
2024-12-15 11:23:25 +0000

    Mostrar marcadores en los lugares en los que hay pérdidas

--- app/src/main/java/org/iesharia/perdidaslanzarote/view/MapScreen.kt
+++ app/src/main/java/org/iesharia/perdidaslanzarote/view/MapScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import com.utsman.osmandcompose.*
+import org.iesharia.perdidaslanzarote.viewmodel.AppViewModel
 import org.osmdroid.tileprovider.tilesource.OnlineTileSourceBase
 import org.osmdroid.tileprovider.tilesource.XYTileSource
 import org.osmdroid.util.GeoPoint
@@ -27,19 +28,35 @@ val GoogleSat: OnlineTileSourceBase = object : XYTileSource(
 }
 
 @Composable
-fun MapScreen() {
+fun MapScreen(appViewModel: AppViewModel) {
     val cameraState = rememberCameraState {
         geoPoint = GeoPoint(29.03668877801322, -13.641841146662419)
-        zoom = 11.5
+        zoom = 11.9
     }
 
     var mapProperties by remember { mutableStateOf(DefaultMapProperties) }
+    val placesWithLostItems by appViewModel.getPlacesWithLostItems().collectAsState(initial = emptyList())
+
     Box(modifier = Modifier.fillMaxSize()) {
         OpenStreetMap(modifier = Modifier.fillMaxSize(), cameraState = cameraState, properties = mapProperties) {
             mapProperties = mapProperties
                 .copy(tileSources = GoogleSat)
                 .copy(isEnableRotationGesture = true)
                 .copy(zoomButtonVisibility = ZoomButtonVisibility.SHOW_AND_FADEOUT)
+
+            placesWithLostItems.forEach { place ->
+                val markerState = rememberMarkerState(
+                    geoPoint = GeoPoint(
+                        place.latitude.toDouble(),
+                        place.longitude.toDouble()
+                    )
+                )
+
+                Marker(
+                    state = markerState,
+                    title = place.name,
+                )
+            }
         }
     }
 }
