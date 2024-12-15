1d9965cba0b16e8522b9f7200f2d46b3b7b6ead3
fmoorenoo <fermontemore@gmail.com>
2024-12-15 11:06:40 +0000

    Query para obtener todos los ítems perdidos en un lugar

--- app/src/main/java/org/iesharia/perdidaslanzarote/model/dao/LostItemDAO.kt
+++ app/src/main/java/org/iesharia/perdidaslanzarote/model/dao/LostItemDAO.kt
@@ -2,10 +2,14 @@ package org.iesharia.perdidaslanzarote.model.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import org.iesharia.perdidaslanzarote.model.entities.LostItem
 
 @Dao
 interface LostItemDao {
     @Insert
     suspend fun insertLostItem(lostItem: LostItem)
+
+    @Query("SELECT * FROM lost_item WHERE placeId = :placeId")
+    suspend fun getLostItemsByPlaceId(placeId: Int): List<LostItem>
 }
