5868373be01862ed75ecbd4eba119c346e2d6a2d
fmoorenoo <fermontemore@gmail.com>
2024-12-15 14:22:49 +0000

    Creado LostItemsScreen.kt para la sección de ver pérdidas

---
+++ app/src/main/java/org/iesharia/perdidaslanzarote/view/LostItemsScreen.kt
@@ -0,0 +1,9 @@
+package org.iesharia.perdidaslanzarote.view
+
+import androidx.compose.runtime.Composable
+import org.iesharia.perdidaslanzarote.viewmodel.AppViewModel
+
+@Composable
+fun LostItemsScreen(appViewModel: AppViewModel) {
+
+}
