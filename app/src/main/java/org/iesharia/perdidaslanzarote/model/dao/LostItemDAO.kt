14addb48a1f7a331b471d0fdb8d3c95f4d21c5e0
fmoorenoo <fermontemore@gmail.com>
2024-12-15 14:38:32 +0000

    Métodos '@Update' y '@Delete' para la tabla 'lost_item' y Query para obtener todas las pérdidas

--- app/src/main/java/org/iesharia/perdidaslanzarote/model/dao/LostItemDAO.kt
+++ app/src/main/java/org/iesharia/perdidaslanzarote/model/dao/LostItemDAO.kt
@@ -1,14 +1,26 @@
 package org.iesharia.perdidaslanzarote.model.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
 import org.iesharia.perdidaslanzarote.model.entities.LostItem
 
 @Dao
 interface LostItemDao {
     @Insert
     suspend fun insertLostItem(lostItem: LostItem)
+
+    @Query("SELECT * FROM lost_item")
+    fun getAllLostItems(): Flow<List<LostItem>>
+
+    @Update
+    suspend fun updateLostItem(lostItem: LostItem)
+
+    @Delete
+    suspend fun deleteLostItem(lostItem: LostItem)
 
     @Query("SELECT * FROM lost_item WHERE placeId = :placeId")
     suspend fun getLostItemsByPlaceId(placeId: Int): List<LostItem>
