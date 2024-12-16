726710cbd3efd6c905a62eb91b5f2cfa9aeda99c
fmoorenoo <fermontemore@gmail.com>
2024-12-16 13:13:50 +0000

    Creado EditLostItem.kt para el 'AlertDialog' que permita modificar una pérdida

---
+++ app/src/main/java/org/iesharia/perdidaslanzarote/view/lostitems/EditLostItem.kt
@@ -0,0 +1,16 @@
+package org.iesharia.perdidaslanzarote.view.lostitems
+
+import androidx.compose.runtime.*
+import org.iesharia.perdidaslanzarote.model.entities.LostItem
+
+@Composable
+fun EditLostItem(
+    lostItem: LostItem,
+    onDismiss: () -> Unit,
+    onSave: (LostItem) -> Unit
+) {
+
+}
+
+
+
