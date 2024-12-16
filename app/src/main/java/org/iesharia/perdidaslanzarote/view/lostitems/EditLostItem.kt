ad6f2afe80276ed68484bd6239257e0132163c60
fmoorenoo <fermontemore@gmail.com>
2024-12-16 13:22:14 +0000

    AlertDialog con iconos para confirmar o cancelar la edición

--- app/src/main/java/org/iesharia/perdidaslanzarote/view/lostitems/EditLostItem.kt
+++ app/src/main/java/org/iesharia/perdidaslanzarote/view/lostitems/EditLostItem.kt
@@ -1,5 +1,12 @@
 package org.iesharia.perdidaslanzarote.view.lostitems
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import org.iesharia.perdidaslanzarote.model.entities.LostItem
 
@@ -9,7 +16,26 @@ fun EditLostItem(
     onDismiss: () -> Unit,
     onSave: (LostItem) -> Unit
 ) {
+    var itemName by remember { mutableStateOf(lostItem.itemName) }
+    var description by remember { mutableStateOf(lostItem.description ?: "") }
+    var contact by remember { mutableStateOf(lostItem.contact) }
 
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        confirmButton = {
+            IconButton(onClick = {
+                onSave(lostItem.copy(itemName = itemName, description = description, contact = contact))
+            }) {
+                Icon(Icons.Default.Check, contentDescription = "Guardar")
+            }
+        },
+        dismissButton = {
+            IconButton(onClick = onDismiss) {
+                Icon(Icons.Default.Clear, contentDescription = "Cancelar")
+            }
+        },
+        title = { Text(text = "Editar pérdida") },
+    )
 }
 
 
