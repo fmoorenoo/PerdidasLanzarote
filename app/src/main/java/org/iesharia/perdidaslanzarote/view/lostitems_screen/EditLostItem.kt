32c55c4678697e35d0685bb7e446d2bbd1d4b528
fmoorenoo <fermontemore@gmail.com>
2024-12-16 13:31:36 +0000

    Campos para modificar la información de una pérdida

--- app/src/main/java/org/iesharia/perdidaslanzarote/view/lostitems_screen/EditLostItem.kt
+++ app/src/main/java/org/iesharia/perdidaslanzarote/view/lostitems_screen/EditLostItem.kt
@@ -1,13 +1,21 @@
 package org.iesharia.perdidaslanzarote.view.lostitems_screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import org.iesharia.perdidaslanzarote.model.entities.LostItem
 
 @Composable
@@ -35,6 +43,29 @@ fun EditLostItem(
             }
         },
         title = { Text(text = "Editar pérdida") },
+        text = {
+            Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+                OutlinedTextField(
+                    value = itemName,
+                    onValueChange = { itemName = it },
+                    label = { Text("Nombre del ítem") },
+                    modifier = Modifier.fillMaxWidth()
+                )
+                OutlinedTextField(
+                    value = description,
+                    onValueChange = { description = it },
+                    label = { Text("Descripción") },
+                    modifier = Modifier.fillMaxWidth()
+                )
+                OutlinedTextField(
+                    value = contact,
+                    onValueChange = { contact = it },
+                    label = { Text("Contacto") },
+                    modifier = Modifier.fillMaxWidth(),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone)
+                )
+            }
+        }
     )
 }
 
