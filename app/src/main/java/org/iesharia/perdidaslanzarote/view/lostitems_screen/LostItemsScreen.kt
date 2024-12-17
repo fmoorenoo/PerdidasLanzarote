bd11ee57cd4dd28ee274987085a928b6de119e63
fmoorenoo <fermontemore@gmail.com>
2024-12-17 11:44:41 +0000

    Reordenado el card con la información de cada pérdida y estilos mejorados

--- app/src/main/java/org/iesharia/perdidaslanzarote/view/lostitems_screen/LostItemsScreen.kt
+++ app/src/main/java/org/iesharia/perdidaslanzarote/view/lostitems_screen/LostItemsScreen.kt
@@ -6,13 +6,17 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
 import androidx.compose.material.icons.filled.Create
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,7 +40,7 @@ fun LostItemsScreen(appViewModel: AppViewModel) {
             text = "Todas las pérdidas",
             fontWeight = FontWeight.Bold,
             fontSize = 24.sp,
-            color = Color.White,
+            color = Color.Black,
             modifier = Modifier.padding(bottom = 8.dp)
         )
         if (lostItems.isEmpty()) {
@@ -44,7 +48,7 @@ fun LostItemsScreen(appViewModel: AppViewModel) {
         }
 
         LazyColumn(
-            verticalArrangement = Arrangement.spacedBy(8.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp),
             modifier = Modifier.fillMaxSize()
         ) {
             items(lostItems) { lostItem ->
@@ -53,26 +57,72 @@ fun LostItemsScreen(appViewModel: AppViewModel) {
                 Card(
                     modifier = Modifier.fillMaxWidth(),
                     shape = RoundedCornerShape(10.dp),
-                    colors = CardDefaults.cardColors(containerColor = Color(0xFFE8E5FF)),
-                    elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+                    colors = CardDefaults.cardColors(containerColor = Color(0xFFDAE4FF)),
+                    elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)
                 ) {
-                    Column(modifier = Modifier.fillMaxWidth().padding(16.dp), verticalArrangement = Arrangement.spacedBy(8.dp)) {
-                        Text(
-                            text = lostItem.itemName,
-                            fontWeight = FontWeight.Bold,
-                            fontSize = 20.sp
-                        )
-                        HorizontalDivider(color = Color.Gray, thickness = 1.dp)
-                        Text(text = "Tipo: ${lostItemType?.name ?: "Sin tipo"}", fontSize = 16.sp)
-                        Text(text = "Lugar: ${place.name}", fontSize = 16.sp)
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                        verticalArrangement = Arrangement.spacedBy(12.dp)
+                    ) {
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            Text(
+                                text = lostItem.itemName,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 24.sp
+                            )
+                            Text(
+                                text = lostItemType?.name ?: "Sin tipo",
+                                fontSize = 24.sp,
+                                color = Color(0xFF5A5A5A),
+                                fontWeight = FontWeight.Medium
+                            )
+                        }
+
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            Row(verticalAlignment = Alignment.CenterVertically) {
+                                Icon(
+                                    imageVector = Icons.Default.LocationOn,
+                                    contentDescription = "Ubi",
+                                    modifier = Modifier.size(25.dp),
+                                    tint = Color(0xFF3A547A)
+                                )
+                                Spacer(modifier = Modifier.width(4.dp))
+                                Text(
+                                    text = place.name,
+                                    fontSize = 24.sp,
+                                    color = Color(0xFF3A547A),
+                                    fontWeight = FontWeight.SemiBold
+                                )
+                            }
+                            Row(verticalAlignment = Alignment.CenterVertically) {
+                                Icon(
+                                    imageVector = Icons.Default.Call,
+                                    contentDescription = "Call",
+                                    modifier = Modifier.size(25.dp),
+                                    tint = Color(0xFF3A547A)
+                                )
+                                Spacer(modifier = Modifier.width(4.dp))
+                                Text(
+                                    text = lostItem.contact,
+                                    fontSize = 20.sp,
+                                    color = Color(0xFF3A547A),
+                                    fontWeight = FontWeight.Medium
+                                )
+                            }
+                        }
                         Text(
                             text = "Descripción: ${lostItem.description ?: "Sin descripción"}",
-                            fontSize = 16.sp,
-                        )
-                        Text(
-                            text = "Contacto: ${lostItem.contact}",
-                            fontSize = 16.sp,
-                            color = Color.Blue
+                            fontSize = 20.sp,
+                            color = Color(0xFF595964),
+                            fontStyle = FontStyle.Italic
                         )
                         Row(
                             modifier = Modifier.fillMaxWidth(),
@@ -95,6 +145,7 @@ fun LostItemsScreen(appViewModel: AppViewModel) {
                                     modifier = Modifier.size(25.dp)
                                 )
                             }
+                            Spacer(modifier = Modifier.width(18.dp))
                             Button(
                                 onClick = { appViewModel.deleteLostItem(lostItem.id) },
                                 colors = ButtonDefaults.buttonColors(containerColor = Color(0xCBFF6767)),
@@ -106,12 +157,13 @@ fun LostItemsScreen(appViewModel: AppViewModel) {
                                 Icon(
                                     imageVector = Icons.Default.Delete,
                                     contentDescription = "Delete",
-                                    modifier = Modifier.size(25.dp),
+                                    modifier = Modifier.size(25.dp)
                                 )
                             }
                         }
                     }
                 }
+
             }
         }
         if (showEditor && itemToEdit != null) {
